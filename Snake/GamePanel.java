1d7b24b15f363ed1a14820a5df53365156212bb5
ScrimaliAnthony <anthony.scrimali@gmail.com>
2023-11-13 15:12:38 +0100

    Class Scoreboard - Gestion de l'affichage du tableau des scores

--- Snake/GamePanel.java
+++ Snake/GamePanel.java
@@ -144,32 +144,21 @@ public class GamePanel extends JPanel implements ActionListener {
         g.setFont(new Font("Arial", Font.BOLD, 100));
         FontMetrics metrics1 = getFontMetrics(g.getFont());
         String gameOverText = "Game Over";
-        g.drawString(gameOverText, (SCREEN_WIDTH - metrics1.stringWidth(gameOverText)) / 2, SCREEN_HEIGHT / 4); // Position plus haute
+        g.drawString(gameOverText, (SCREEN_WIDTH - metrics1.stringWidth(gameOverText)) / 2, SCREEN_HEIGHT / 4);
     
-        // Position de départ pour le tableau des scores
-        int y = SCREEN_HEIGHT / 2; 
+        // Appel de la méthode afficherScores pour dessiner le tableau des scores
+        int yStart = SCREEN_HEIGHT / 2; 
+        int y = scoreboard.afficherScores(g, SCREEN_WIDTH, yStart);
     
-        // Affichage du tableau des scores
-        g.setColor(Color.WHITE);
-        g.setFont(new Font("Arial", Font.BOLD, 40));
-        for (int score : scoreboard.getHighScores()) {
-            g.drawString("Score: " + score, (SCREEN_WIDTH - g.getFontMetrics().stringWidth("Score: " + score)) / 2, y);
-            y += g.getFontMetrics().getHeight();
-        }
-    
-        // Espace entre le tableau des scores et le score du joueur
-        y += g.getFontMetrics().getHeight(); // Double saut de ligne
+        // Espace supplémentaire entre le tableau des scores et le score du joueur
+        y += g.getFontMetrics().getHeight(); // Ajustement pour plus d'espace
     
         // Affichage du score actuel du joueur
         g.setColor(Color.WHITE);
         g.setFont(new Font("Arial", Font.BOLD, 30));
-        FontMetrics metrics2 = getFontMetrics(g.getFont());
         String scoreText = "Votre Score: " + applesEaten;
-        g.drawString(scoreText, (SCREEN_WIDTH - metrics2.stringWidth(scoreText)) / 2, y);
+        g.drawString(scoreText, (SCREEN_WIDTH - g.getFontMetrics().stringWidth(scoreText)) / 2, y);
     }
-    
-    
-    
 
     @Override
     public void actionPerformed(ActionEvent e) {
--- Snake/Scoreboard.java
+++ Snake/Scoreboard.java
@@ -3,6 +3,10 @@ package Snake;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
 public class Scoreboard {
     private ArrayList<Integer> highScores;
 
@@ -21,4 +25,17 @@ public class Scoreboard {
     public ArrayList<Integer> getHighScores() {
         return new ArrayList<>(highScores); // Retourne une copie de la liste des scores
     }
+
+    public int afficherScores(Graphics g, int screenWidth, int yStart) {
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 40));
+        int y = yStart;
+        for (int score : highScores) {
+            String scoreText = "Score: " + score;
+            g.drawString(scoreText, (screenWidth - g.getFontMetrics().stringWidth(scoreText)) / 2, y);
+            y += g.getFontMetrics().getHeight();
+        }
+        return y; // Retourne la position y finale
+    }
+    
 }
