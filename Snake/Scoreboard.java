9ebfd4e084a9139dc2cf0e232037dd5fd46287ba
ScrimaliAnthony <anthony.scrimali@gmail.com>
2023-11-14 16:00:33 +0100

    Ajout de 'new best score' si le record est battu

--- Snake/Scoreboard.java
+++ Snake/Scoreboard.java
@@ -9,17 +9,21 @@ import java.awt.Graphics;
 
 public class Scoreboard {
     private ArrayList<Integer> highScores;
+    private boolean isNewBestScore; // Indicateur pour un nouveau meilleur score
 
     public Scoreboard() {
         highScores = new ArrayList<>();
+        isNewBestScore = false; // Initialisé à faux
     }
 
     public void addScore(int score) {
+        boolean wasFirst = highScores.isEmpty() || score > highScores.get(0);
         highScores.add(score);
         Collections.sort(highScores, Collections.reverseOrder());
         if (highScores.size() > 5) {
             highScores.remove(5); // Conserve uniquement les 5 meilleurs scores
         }
+        isNewBestScore = wasFirst && score == highScores.get(0); // Vérifie si c'est un nouveau meilleur score
     }
 
     public ArrayList<Integer> getHighScores() {
@@ -32,6 +36,10 @@ public class Scoreboard {
         int y = yStart;
         for (int score : highScores) {
             String scoreText = "Score: " + score;
+            if (isNewBestScore && highScores.indexOf(score) == 0) {
+                scoreText += " - new best score!";
+                isNewBestScore = false; // Réinitialiser après l'affichage
+            }
             g.drawString(scoreText, (screenWidth - g.getFontMetrics().stringWidth(scoreText)) / 2, y);
             y += g.getFontMetrics().getHeight();
         }
